fde5553a243e293d83af60601469755a0bc3afa5
Tobias Jost <tobias.jost@spinweb.de>
2016-04-17 08:50:38 +0200

    Everything works... in Debug mode

--- modules/cli/main.cpp
+++ modules/cli/main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <cassert>
 
 #include <sudokusolver/solver.hpp>
 
@@ -7,54 +8,41 @@ int main(int argc, char* argv[])
 {
     using namespace SudokuSolver;
 
-    /*SudokuSolver::Core::Cell cell;
+    std::string str;
 
-    std::cout << "Cell state: " << cell.state() << ", value: " << cell.value() << std::endl;
+    if(argc > 1)
+    {
+        str = std::string(argv[1]);
+    }
+    else
+    {
+        std::cout << "Please pass Sudoku string as command line argument. Aborting!" << std::endl;
+        //str = "001230000004000000003000451000302006500040002100507000869000700000000800000095100";
+        return EXIT_FAILURE;
+    }
 
     try
     {
-        while(cell.hasNextValue())
+        Core::Board board = Core::Board::Parser::fromStdString(str);
+
+        std::cout << "Solving Sudoku..." << std::endl;
+        std::cout << "INPUT:  " << Core::Board::Parser::toStdString(board) << std::endl;
+        Core::Solver::AnalyzationResultType result = Core::Solver::solve(board);
+
+        if(Core::Solver::RESULT_VALID == result)
         {
-            cell.setNextValue();
-            std::cout << "Cell state: " << cell.state() << ", value: " << cell.value() << std::endl;
+            std::cout << "OUTPUT: " <<Core::Board::Parser::toStdString(board) << std::endl;
         }
+        else if(Core::Solver::RESULT_INVALID == result)
+        {
+            std::cout << "Sudoku is not solvable." << std::endl;
+        }
+        assert(Core::Solver::RESULT_INDETERMINATE != result);
+
+        return EXIT_SUCCESS;
     }
-    catch(std::exception& ex)
+    catch(std::exception ex)
     {
-        const char* msg = ex.what();
-        std::cerr << "[FATAL ERROR] " << msg << std::endl;
-        return EXIT_FAILURE;
-    }*/
-
-    std::string str = "X9X" "XXX" "24X"
-                      "5X6" "XXX" "9XX"
-                      "X37" "6X9" "85X"
-
-                      "XX2" "X6X" "X7X"
-                      "968" "7X4" "XXX"
-                      "4X3" "1XX" "XXX"
-
-                      "XXX" "48X" "XX3"
-                      "XXX" "2X7" "XX5"
-                      "XXX" "XX1" "XXX";
-
-    /*std::string str = "891" "375" "246"
-                      "546" "812" "937"
-                      "237" "649" "851"
-
-                      "152" "963" "478"
-                      "968" "754" "312"
-                      "473" "128" "569"
-
-                      "725" "486" "193"
-                      "314" "297" "685"
-                      "689" "531" "724";*/
-
-    Core::Board board = Core::Board::Parser::fromStdString(str);
-
-    std::cout << Core::Board::Parser::toStdString(board) << std::endl;
-    Core::Solver::solve(board);
-    std::cout << Core::Board::Parser::toStdString(board) << std::endl;
-
-    return EXIT_SUCCESS;
+        std::cout << "ERROR: " << ex.what() << std::endl;
+    }
 }
